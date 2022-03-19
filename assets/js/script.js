ffb3595de210b814156eefc725f599d574f4f8aa
Fernando Almeida <nando.lalmeida@gmail.com>
2022-03-19 12:12:35 -0400

    added initial logic for replacing the welcome text with questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-// Timer related functions
 var startTimer = document.querySelector("#btn");
 
 // Querying the first DIV inside the section with id="page-quizlet"
-var pageDivEl = document.querySelector("#page-quizlet");
+var pageDivElement = document.querySelector("#page-quizlet");
 
 
 // Set interval 
@@ -31,30 +30,37 @@ setTimer = function(duration, display) {
     }, 1000);
 };
 
+clearDivElement = function () {
+    // pageDivElement.remove();
+    createQuestionsElements();
+}
 
 createQuestionsElements = function () {
-    // Define div classes name
+//#region vars 
+    // create and define div classes name
     var pageWrapperContent = document.createElement("div");
-    pageWrapperContent.className = "page-wrapper-content"; 
-    
-    // Creating the H2 element and adding it to the page-wrapper-content
-    var pageH2El = document.createElement("h2");
-    pageH2El.style.textAlign = "left";
-    pageH2El.className = "page-welcome-txt";
-    pageH2El.textContent = "Coding Quiz Challenge 222huahsuhduahushdasuhdu ashdasuhdashhdasudhsauhsau    22";
-    pageWrapperContent.appendChild(pageH2El);
-    pageDivEl.parentElement.replaceChild(pageWrapperContent, pageDivEl);
+    pageWrapperContent.className = "page-wrapper-content-swap"; 
 
-    // create page buttons div
+
+    // Creating the H2 element and adding it to the page-wrapper-content
+    var pageH2TagElement = document.createElement("h2");
+    pageH2TagElement.style.textAlign = "left";
+    pageH2TagElement.className = "page-welcome-txt";
+    pageWrapperContent.appendChild(pageH2TagElement);
+
+//#endregion vars
+
+//#region buttons
+    // create page buttons
     var pageButtonWrapper = document.createElement("div");
     pageButtonWrapper.className = "btn-wrapper";
 
     // assigns the new buttons   
     var buttons = [
-        "Answer1",
-        "Answer2",
-        "Answer3",
-        "Answer4",
+        "answer1",
+        "answer2",
+        "answer3",
+        "answer4",
     ];
 
     // creating buttons and append to pageButtons div
@@ -67,6 +73,61 @@ createQuestionsElements = function () {
         pageButtonWrapper.appendChild(pageButtonElement);
         pageWrapperContent.appendChild(pageButtonWrapper);
     };
+//#endregion buttons
+
+    pageDivElement.parentElement.replaceChild(pageWrapperContent, pageDivElement);
+
+
+    // console.log("inside create elements" + pageWrapperContent);
+    // return pageWrapperContent;
+    
+    createRandomQuestions(pageWrapperContent);
+};
+
+
+createRandomQuestions = function (pageWrapperContent, question1, question2) {
+    var content = pageWrapperContent;
+    // var obj =JSON.parse(content);
+    console.log(content);
+    // debugger;
+    //#region questions
+    var questionsObject = {
+        question1: "What is your name?",
+        q1Button1: "x",
+        q1Button2: "y",
+        q1Button3: "z",
+        q1Button4: "k",
+
+        question2: "What is your city?",
+        q2Button1: "a",
+        q2Button2: "b",
+        q2Button3: "c",
+        q2Button4: "d",
+    };
+    console.log(questionsObject);
+//#endregion questions
+
+    // console.log(content.querySelector("h2"));
+    var h2TagContent = content.querySelector("h2");
+    var button1 = content.querySelector("#btn-answer1");
+    var button2 = content.querySelector("#btn-answer2");
+    var button3 = content.querySelector("#btn-answer3");
+    var button4 = content.querySelector("#btn-answer4");
+    // console.log(h2TagContent);
+    console.log(button1);
+
+    // // console.log(h2TagContent);
+    // if (h2TagContent == true) {
+    //         console.log(h2TagContent);
+            h2TagContent.textContent = "Test 1";
+            button1.innerHTML = "test1";
+            button2.innerHTML = "test2";
+            button3.innerHTML = "test3";
+            button4.innerHTML = "test4";
+    // }
+    
+    // console.log("Show the content from createQuestionsElements function " + content);
+
 };
 
 startQuiz = function () {
@@ -75,7 +136,8 @@ startQuiz = function () {
 
     // send values to setTimer function
     setTimer(duration, display);
-    createQuestionsElements();
+    clearDivElement();
+    // createQuestionsElements();
 };
 
 startTimer.addEventListener("click", startQuiz);
